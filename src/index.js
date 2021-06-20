3dd9093d7a50db7e26c8f97e894ee45a67867f6f
Satoshi T <64382011+Satoshi-Tb@users.noreply.github.com>
2021-06-20 19:08:41 +0900

    Add Back Function

--- src/index.js
+++ src/index.js
@@ -2,6 +2,20 @@ import "./styles.css";
 
 const onClickAdd = () => {
   const inputText = document.getElementById("add-text").value;
+  createIncompleteList(inputText);
+};
+
+// 未完了リストから指定の要素を削除
+const deleteFromIncompleteList = (target) => {
+  document.getElementById("incomplete-list").removeChild(target);
+};
+
+const deleteFromCompleteList = (target) => {
+  document.getElementById("complete-list").removeChild(target);
+};
+
+//未完了リストに追加する関数
+const createIncompleteList = (text) => {
   document.getElementById("add-text").value = "";
 
   // li生成
@@ -12,7 +26,7 @@ const onClickAdd = () => {
   div.className = "list-row";
 
   const p = document.createElement("p");
-  p.innerText = inputText;
+  p.innerText = text;
 
   const completeButton = document.createElement("button");
   completeButton.innerText = "完了";
@@ -33,7 +47,13 @@ const onClickAdd = () => {
 
     const backButton = document.createElement("button");
     backButton.innerText = "戻す";
-    backButton.addEventListener("click", () => {});
+    backButton.addEventListener("click", () => {
+      //押された戻すボタンの親の親タグ(li)を完了リストから削除
+      deleteFromCompleteList(backButton.parentNode.parentNode);
+
+      const text = backButton.parentNode.firstElementChild.innerText;
+      createIncompleteList(text);
+    });
 
     li.appendChild(addTarget);
     addTarget.appendChild(p);
@@ -54,14 +74,8 @@ const onClickAdd = () => {
   div.appendChild(deleteButton);
 
   document.getElementById("incomplete-list").appendChild(li);
-
-  //console.log(li);
 };
 
-// 未完了リストから指定の要素を削除
-const deleteFromIncompleteList = (target) => {
-  document.getElementById("incomplete-list").removeChild(target);
-};
 document
   .getElementById("add-button")
   .addEventListener("click", () => onClickAdd());
